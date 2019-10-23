dd353e6ddf81020551e146fef3114a6662c5e7b0
dannyseymour <daniel@discordia.io>
2019-10-23 14:45:00 -0600

    Initial commit

---
+++ src/main/java/io/discordia/floobankservlet/FloobankservletApplication.java
@@ -0,0 +1,13 @@
+package io.discordia.floobankservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloobankservletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FloobankservletApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/discordia/floobankservlet/FloobankservletApplicationTests.java
@@ -0,0 +1,13 @@
+package io.discordia.floobankservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloobankservletApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
